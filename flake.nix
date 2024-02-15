1e5ebb9a762e2aeb9541ec9977673af56c392cf2
Nate Jensvold <jensvoldnate@gmail.com>
2024-02-14 21:31:40 -0800

    Added brew util commands, updated repo flake, added brew install

--- flake.nix
+++ flake.nix
@@ -13,6 +13,22 @@
       {
         # The default shell
         devShells.default = pkgs.mkShell {
+          shellHook = ''
+            NOCOLOR='\033[0m'
+            RED='\033[0;31m'
+
+            source ./scripts/ubuntu/install.sh
+            ssh_support
+            ssh_status=$?
+
+            if [ ''$ssh_status -eq 1 ];
+            then
+              printf "''${RED}%s''${NOCOLOR}\n" "Github SSH support not detected, add a private key to ~/.ssh, or follow the directions in this link
+              https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account"
+            fi
+          ''
+          ;
+          PWSH_PATH = "${pkgs.powershell}/bin/pwsh";
           packages = with pkgs; [
             # Nix development packages
             nixpkgs-fmt
