befe6548840a2a848cce71fe4cf52b5f9a79eaeb
Nate Jensvold <jensvoldnate@gmail.com>
2024-03-03 20:32:28 -0800

    Removed or renamed old nix files for user and host setup

--- settings/nix/home/users/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-let
-  userPackagesPath = ./users + "/${config.user.name}/$packages.nix";
-in
-{
-  # home.packages = with pkgs; (
-  #   lib.optional (builtins.pathExists userPackagesPath) import userPackagesPath { inherit pkgs; }
-  # );
-}
