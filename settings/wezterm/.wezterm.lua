6d1e3f527a42f8862ff3a1623c45df409637de64
Nate Jensvold <jensvoldnate@gmail.com>
2024-03-04 23:43:06 -0800

    Added wezterm background

--- settings/wezterm/.wezterm.lua
+++ settings/wezterm/.wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,9 +9,31 @@ local config = wezterm.config_builder()
 local is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
 if is_windows then
     config.default_domain = 'WSL:Ubuntu'
+
+    local user = os.getenv("UserProfile")
+    config.window_background_image = string.format("%s/wez_backgrounds/castle-background.jpg", user)
+    -- wezterm.log_info(string.format("%s", user))
+
 end
 
 config.window_close_confirmation = "NeverPrompt"
+
+config.color_scheme = 'Batman'
+-- config.window_background_opacity = 0.90
+
+config.window_background_image_hsb = {
+    -- Darken the background image by reducing it to 1/3rd
+    brightness = 0.1,
+    -- You can adjust the hue by scaling its value.
+    -- a multiplier of 1.0 leaves the value unchanged.
+    hue = 0.5,
+    -- You can adjust the saturation also.
+    saturation = 2.0,
+  }
+
+config.keys = {
+    { key = 't', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain'},
+  }
 
 -- and finally, return the configuration to wezterm
 return config
