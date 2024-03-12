7d787b6ec09da20a897cc0d26cb2905127753167
Nate Jensvold <jensvoldnate@gmail.com>
2024-03-12 05:55:01 +0000

    Moved linuxbrew sourcing to host file instead of hardware

--- nix/hosts/amazon-clouddesk/files/default.nix
+++ nix/hosts/amazon-clouddesk/files/default.nix
@@ -5,4 +5,5 @@ in
 {
   # Dynamic files
   home.file.".vscode-server/data/Machine/java_settings.xml".source = config.lib.meta.mkMutableSymlink "/vscode/work/java_settings.xml";
+  home.file.".vscode-server/data/Machine/settings.json".source = config.lib.meta.mkMutableSymlink "/vscode/settings.json";
 }
--- nix/hosts/home/home.nix
+++ nix/hosts/home/home.nix
@@ -1,5 +1,12 @@
 { config, lib, pkgs, inputs, user, hostname, location, secrets, dotfiles, ... }:
+let
+in
 {
+  programs.zsh.initExtra = ''
+    # Source brew path, should be present if system was setup by bash https://raw.githubusercontent.com/JateNensvold/environment/master/scripts/ubuntu/install.sh
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  '';
+
   home.packages = with pkgs; [
   ] ++
   (with pkgs.custom; [
