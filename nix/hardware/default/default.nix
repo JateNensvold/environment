893c12bf47435cf79192542d9d1440e53749fb9f
Nate Jensvold <jensvold@amazon.com>
2024-03-12 05:55:01 +0000

    Moved linuxbrew sourcing to host file instead of hardware

--- nix/hardware/default/default.nix
+++ nix/hardware/default/default.nix
@@ -2,8 +2,5 @@
 let
 in
 {
-  programs.zsh.initExtra = ''
-    # Source brew path, should be present if system was setup by bash https://raw.githubusercontent.com/JateNensvold/environment/master/scripts/ubuntu/install.sh
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  '';
+
 }
