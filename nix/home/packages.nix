a1f8c6976889d86c3b9e2d1bdc2bc202a408d720
Nate Jensvold <jensvoldnate@gmail.com>
2024-03-16 23:30:11 -0700

    Added cargo to nix packages, updated wezterm to allow ctrl-c in nvim

--- nix/home/packages.nix
+++ nix/home/packages.nix
@@ -4,6 +4,7 @@
   home.packages = with pkgs; [
     ansible
     autojump
+    cargo
     cheat
     curl
     delta
--- settings/wezterm/.wezterm.lua
+++ settings/wezterm/.wezterm.lua
@@ -18,7 +18,7 @@ end
 
 config.window_close_confirmation = "NeverPrompt"
 
-config.color_scheme = 'Batman'
+-- config.color_scheme = 'Batman'
 -- config.window_background_opacity = 0.90
 
 config.window_background_image_hsb = {
@@ -29,11 +29,31 @@ config.window_background_image_hsb = {
     hue = 0.5,
     -- You can adjust the saturation also.
     saturation = 2.0,
-  }
+}
 
 config.keys = {
-    { key = 't', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain'},
-  }
+  -- { key = 't', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain'},
+  { key = 'v', mods = 'CTRL', action = wezterm.action{PasteFrom='Clipboard'}},
+  { key = 'c', mods = 'CTRL', action = wezterm.action{CopyTo='Clipboard'}},
+  { key="c", mods="CTRL", action = wezterm.action_callback(function(window, pane)
+      local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+      if has_selection then
+        window:perform_action(
+          wezterm.action{CopyTo="ClipboardAndPrimarySelection"},
+          pane)
+        window:perform_action("ClearSelection", pane)
+      else
+        window:perform_action(
+          wezterm.action{SendKey={key="c", mods="CTRL"}},
+          pane)
+      end
+    end)
+  },
+  { key="c", mods="CTRL|SHIFT", action = wezterm.action{SendKey={key="c", mods="CTRL"}}},
+  { key="v", mods="CTRL", action= wezterm.action{PasteFrom='Clipboard'} },
+  { key = 'v', mods = 'CTRL', action = wezterm.action{PasteFrom='Clipboard'}},
+  { key="v", mods="CTRL|SHIFT", action=wezterm.action{SendKey={key="v", mods="CTRL"}} }
+}
 
 -- and finally, return the configuration to wezterm
 return config
