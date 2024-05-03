e40ce75ce50d6e154d6a8b73b219b613412d060f
Nate Jensvold <jensvoldnate@gmail.com>
2024-05-03 13:21:13 -0700

    Fixed darwin-rebuild command in install.sh

--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -121,7 +121,7 @@
 
 		cd ~/environment/nix
 		if [ "$OS_TYPE" = "Darwin" ]; then
-			nix run --extra-experimental-features nix-command --extra-experimental-features "${homeManagerConfigurationCommandSuffix[@]}"
+			nix run --extra-experimental-features nix-command --extra-experimental-features -- "${homeManagerConfigurationCommandSuffix[@]}"
 
 			info "darwin: home-manager is configured! Here is what we have:"
 			darwin-version
