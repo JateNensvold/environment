39527602c916df30a8ae15ee99c983e5371404d9
Nate Jensvold <jensvoldnate@gmail.com>
2024-05-07 12:16:37 -0700

    Updated aeroplane config for darwin

--- nix/hosts/amazon-laptop/darwin/default.nix
+++ nix/hosts/amazon-laptop/darwin/default.nix
@@ -10,15 +10,12 @@ in {
     dock.enable = true;
     dock.entries = [
       { path = "${pkgs.custom.wezterm}/Applications/WezTerm.app"; }
+      { path = "/Applications/Slack.app"; }
+      { path = "/Applications/Firefox.app"; }
+      { path = "/Applications/Amazon Chime.app"; }
+      { path = "/Applications/Microsoft Outlook.app"; }
       { path = "${pkgs.spotify}/Applications/Spotify.app"; }
       { path = "${pkgs.obsidian}/Applications/Obsidian.app"; }
-      { path = "/Applications/Slack.app"; }
-      {
-        path = "/Applications/Firefox.app";
-      }
-      # { path = "/Applications/Cisco/Cisco AnyConnect Secure Mobility Client.app"; }
-      { path = "/Applications/Microsoft Outlook.app"; }
-      { path = "/Applications/Amazon Chime.app"; }
     ];
   };
 }
