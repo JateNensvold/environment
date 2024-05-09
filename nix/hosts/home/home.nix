12149ef9563c66d55d06fc8aad87e2d5c858679b
Nate Jensvold <jensvoldnate@gmail.com>
2024-05-09 23:29:10 +0000

    Added nvim-window-picker, moved codium nvim plugin to only install on home profile to remove the annoying login popup on other devices

--- nix/hosts/home/home.nix
+++ nix/hosts/home/home.nix
@@ -1,1 +1,9 @@
-{ ... }: { }
+{ config, ... }:
+let
+  link = config.lib.file.mkOutOfStoreSymlink;
+  dotfilePath = "${config.home.homeDirectory}/environment/dotfiles";
+in {
+  # Link work specific nvim files into nvim source
+  home.file."environment/dotfiles/nvim/default/lua/tosh-vim/lazy/home-codium.lua".source =
+    link "${dotfilePath}/nvim/home/codium.lua";
+}
