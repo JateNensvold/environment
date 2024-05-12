71610f7b96fb23839231a02fb0c3e444231c3c5b
Nate Jensvold <jensvoldnate@gmail.com>
2024-05-11 19:23:56 -0700

    Fixed codeium nvim path on home hosts

--- nix/hosts/home/home.nix
+++ nix/hosts/home/home.nix
@@ -4,6 +4,6 @@ let
   dotfilePath = "${config.home.homeDirectory}/environment/dotfiles";
 in {
   # Link work specific nvim files into nvim source
-  home.file."environment/dotfiles/nvim/default/lua/tosh-vim/lazy/home-codium.lua".source =
-    link "${dotfilePath}/nvim/home/codium.lua";
+  home.file."environment/dotfiles/nvim/default/lua/tosh-vim/lazy/home-codeium.lua".source =
+    link "${dotfilePath}/nvim/home/codeium.lua";
 }
