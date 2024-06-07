c0ff39ed7f05360d727f550a7897225e996c02c3
Nate Jensvold <jensvoldnate@gmail.com>
2024-06-07 02:20:38 +0000

    Fix visual mode keybinds for spectre.nvim and tmux sessionizer

--- dotfiles/nvim/default/lua/tosh-vim/lazy/nvim-spectre.lua
+++ dotfiles/nvim/default/lua/tosh-vim/lazy/nvim-spectre.lua
@@ -1,6 +1,5 @@
 return {
 	"nvim-pack/nvim-spectre",
-
 	config = function()
 		vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
 			desc = "Specter: Search current word",
@@ -9,6 +8,9 @@ return {
 			desc = "Specter: Search current word",
 		})
 		vim.keymap.set("n", "<leader>sf", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+			desc = "Specter: Search on current file",
+		})
+		vim.keymap.set("v", "<leader>sf", ':lua require("spectre").open_file_search()<CR>', {
 			desc = "Specter: Search on current file",
 		})
 	end,
