91b1156b9a800c336ec328b2324e0288706251a2
Nate Jensvold <jensvoldnate@gmail.com>
2026-04-14 02:30:06 +0000

    Apply nixfmt and use config.home.homeDirectory in darwin activation
    Replace $HOME with ${config.home.homeDirectory} for consistent
    nix evaluation in the claude settings merge activation script.

--- nix/modules/darwin/home-manager/default.nix
+++ nix/modules/darwin/home-manager/default.nix
@@ -1,22 +1,27 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   dotfilePath = "${config.home.homeDirectory}/environment/dotfiles";
-in {
+in
+{
   home.activation = {
 
-    rsync-home-manager-applications =
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        apps_source="${config.home.homeDirectory}/Applications/Home Manager Apps/"
-        echo "$apps_source"
-        ls -la "$apps_source"
+    rsync-home-manager-applications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      apps_source="${config.home.homeDirectory}/Applications/Home Manager Apps/"
+      echo "$apps_source"
+      ls -la "$apps_source"
 
-        moniker="Nix Trampolines"
-        app_target_base="$HOME/Applications"
-        app_target="$app_target_base/$moniker"
-        mkdir -p "$app_target"
-        ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
+      moniker="Nix Trampolines"
+      app_target_base="$HOME/Applications"
+      app_target="$app_target_base/$moniker"
+      mkdir -p "$app_target"
+      ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
 
-      '';
+    '';
 
   };
 }
