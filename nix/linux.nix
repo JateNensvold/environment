5d4cca46d393e169b9d3289befbe2281776ada4d
Nate Jensvold <jensvoldnate@gmail.com>
2026-04-18 23:13:52 +0000

    Adjust Linux Nix configuration

--- nix/linux.nix
+++ nix/linux.nix
@@ -4,8 +4,10 @@ let
   hostPath = "./hosts/";
 in {
 
-  xdg.configFile."nix/nix.conf ".text = ''
+  xdg.configFile."nix/nix.conf".text = ''
     experimental-features = nix-command flakes
+    # Avoid "download buffer is full" warnings for large downloads.
+    download-buffer-size = 524288000
   '';
 
   imports = [
@@ -18,7 +20,8 @@ in {
   home.packages = [ ]
     ++ (import ./${modulePath}/home/packages.nix { inherit pkgs stablePkgs; })
     ++ (with pkgs; [
-      custom.unocss
-      custom.unocss-lsp
+      # Disabled: upstream unocss packaging currently fails during eval/build.
+      # custom.unocss
+      # custom.unocss-lsp
     ]);
 }
